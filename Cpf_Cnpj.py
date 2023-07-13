b612cf2a7a481c36ce882e6370a838dca8de4156
RA4Z <rraz639@gmail.com>
2023-07-13 15:41:27 -0300

    Validando CNPJ

---
+++ Cpf_Cnpj.py
@@ -0,0 +1,44 @@
+from validate_docbr import CPF, CNPJ
+
+class CpfCnpj:
+    def __init__(self, documento, tipo_documento):
+        self.tipo_documento = tipo_documento
+        documento = str(documento)
+
+        if tipo_documento == 'cpf':
+            if self.cpf_eh_valido(documento):
+                self.cpf = documento
+            else:
+                raise ValueError('CPF Inválido!!')
+            
+        elif self.tipo_documento == 'cnpj':
+            if self.cnpj_eh_valido(documento):
+                self.cnpj = documento
+            else:
+                raise ValueError('CNPJ Inválido!!')
+            
+        else:
+            raise ValueError('Tipo de Documento Inválido!!')
+
+    def __str__(self):
+        return self.format_cpf()
+
+    def cpf_eh_valido(self, documento):
+        if len(documento) == 11:
+            validador = CPF()
+            return validador.validate(documento)
+        else:
+            raise ValueError('Quantidade de dígitos inválida!!')
+        
+    def cnpj_eh_valido(sel,documento):
+        if len(documento) == 14:
+            validador = CNPJ()
+            return validador.validate(documento)
+        else:
+            raise ValueError('Quantidade de dígitos inválida!!')
+    
+    def format_cpf(self):
+        mascara = CPF()
+        return mascara.mask(self.cpf)
+    
+            
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from Cpf import Cpf
+from Cpf_Cnpj import CpfCnpj
+#cpf = Cpf('12656738962')
+#print(cpf)
 
-cpf = Cpf('12656738962')
-
-print(cpf)
+exemplo_cnpj = '35379838000112'
+documento = CpfCnpj(exemplo_cnpj, 'cnpj')
+print(documento)
