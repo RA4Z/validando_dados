f3a5ad2d181b05aec96cce459e6292348e4bb63f
RA4Z <rraz639@gmail.com>
2023-07-14 14:22:47 -0300

    Retornando respostas da API

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from acesso_cep import BuscaEndereco
 
 cep = '89275000'
 objeto_cep = BuscaEndereco(cep)
-print(objeto_cep.acessa_via_cep())
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+print(bairro, cidade, uf)
+
 
 
 """cadastro = DatasBr()
