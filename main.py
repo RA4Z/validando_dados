88537d6f7cfb7496350b844f50c39609865b322c
RA4Z <rraz639@gmail.com>
2023-07-14 10:47:08 -0300

    Validando E-mail com Regex

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from Cpf_Cnpj import Documento
+import re
 
-exemplo_cnpj = '35379838000112'
+padrao = '\w{5,50}@[a-z]{3,10}.com(.br)?'
+texto = 'rraz639@gmail.com.br'
+respostas = re.search(padrao, texto)
+print(respostas.group())
+
+
+"""exemplo_cnpj = '35379838000112'
 exemplo_cpf = '12656738962'
 
 documento = Documento.cria_documento(exemplo_cnpj)
-print(documento)
+print(documento)"""
