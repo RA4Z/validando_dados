90a61d4afc1796be8504a448722c8c7aa31b5237
RA4Z <rraz639@gmail.com>
2023-07-14 11:07:53 -0300

    Formatando apresentação do telefone

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from Cpf_Cnpj import Documento
 from TelefonesBr import TelefonesBr
 import re
 
-telefone = '5547999585728'
+telefone = '554799585728'
 telefone_objeto = TelefonesBr(telefone)
 
+print(telefone_objeto)
 
 """exemplo_cnpj = '35379838000112'
 exemplo_cpf = '12656738962'
